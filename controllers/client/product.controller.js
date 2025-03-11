0325aa32dd10eb1cf736485d11520e1da4cae246
Dương Thành Sơn <duongthanhson2004@gmail.com>
2025-03-11 10:33:02 +0700

    first commit

---
+++ controllers/client/product.controller.js
@@ -0,0 +1,7 @@
+module.exports.index = (req, res) => {
+    res.render("client/pages/products/index.pug");
+}
+
+module.exports.edit = (req, res) => {
+    res.render("client/pages/products/index.pug");
+}
